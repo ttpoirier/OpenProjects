ebab3b1f96b0860bcf3937204f26fa1e1257bb13
ttpoirier <ttpoirier@gmail.com>
2015-03-13 10:31:23 -0400

    Initial Commit
    Commit for test

---
+++ ConnectMyStuff/ConnectMyStuff/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ConnectMyStuff
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFind = new System.Windows.Forms.Button();
+            this.txtZipCode = new System.Windows.Forms.TextBox();
+            this.txtGasPrice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDistance = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(409, 67);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 0;
+            this.btnFind.Text = "Find!";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // txtZipCode
+            // 
+            this.txtZipCode.Location = new System.Drawing.Point(53, 67);
+            this.txtZipCode.Name = "txtZipCode";
+            this.txtZipCode.Size = new System.Drawing.Size(100, 20);
+            this.txtZipCode.TabIndex = 1;
+            // 
+            // txtGasPrice
+            // 
+            this.txtGasPrice.Location = new System.Drawing.Point(53, 121);
+            this.txtGasPrice.Name = "txtGasPrice";
+            this.txtGasPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtGasPrice.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Zip Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Gas Price";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Distance";
+            // 
+            // txtDistance
+            // 
+            this.txtDistance.Location = new System.Drawing.Point(53, 177);
+            this.txtDistance.Name = "txtDistance";
+            this.txtDistance.Size = new System.Drawing.Size(100, 20);
+            this.txtDistance.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 333);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDistance);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGasPrice);
+            this.Controls.Add(this.txtZipCode);
+            this.Controls.Add(this.btnFind);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.TextBox txtZipCode;
+        private System.Windows.Forms.TextBox txtGasPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDistance;
+    }
+}
+
---
+++ ConnectMyStuff/ConnectMyStuff/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConnectMyStuff
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Station
+        {
+            public string stationName { get; set; }
+            public string streetAddress { get; set; }
+            public float gasPrice { get; set; }
+            public float distance { get; set; }
+            public int ranking { get; set; }
+            public int indexNumber { get; set; }
+            public Guid stationId { get; set; }
+
+            public Station(System.Guid Id)
+            {
+                stationId = Id;
+
+            }
+            public float GetDistanceFromTarget()
+            {
+                var distance = 10;
+                return distance;
+            }
+            public float GetPrice()
+            {
+                float price = 3;
+                return price;
+            }
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            var userZipCode = txtZipCode.Text;
+
+            if(userZipCode == "") {
+                MessageBox.Show("Please enter a ZipCode", "Invalid ZipCode", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else {
+                var station = new Station(System.Guid.NewGuid());
+                txtDistance.Text = station.GetDistanceFromTarget().ToString();
+                txtGasPrice.Text = station.GetPrice().ToString();
+            }
+        }
+
+    }
+}
